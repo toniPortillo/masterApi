16979abf17cc72254b5295c0483380fc7c8a9837
toniPortillo <toniplsec@gmail.com>
2020-01-09 22:24:20 +0100

    Implementado findOne

--- router/controllers/lagerSts.js
+++ router/controllers/lagerSts.js
@@ -9,5 +9,13 @@ module.exports = (database) => {
     database.open(sql, [], false, res);
   }
 
-  return { showLagerSts };
+  const findOneLagerSt = (req, res, next) => {
+    const sql = lagerSts.findOne();
+    console.log(sql);
+    console.log(req.query.lag_referenz)
+    let lag_referenz = parseInt(req.query.lag_referenz);
+    database.open(sql, [lag_referenz], false, res)
+  }
+
+  return {showLagerSts, findOneLagerSt};
 }
--- routes/lagerSts.js
+++ routes/lagerSts.js
@@ -6,5 +6,6 @@ const lagerStController = require('../router/controllers/index');
 //const showLagerSts = lagerStController.showLagerSts;
 const lagerSts = lagerStController.lagerSt;
 router.get('/', lagerSts.showLagerSts);
+router.get('/findone', lagerSts.findOneLagerSt);
 
 module.exports = router;
