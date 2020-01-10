58a97b315e96cbc6809e9b5bc657d35eb6800e1a
toniPortillo <toniplsec@gmail.com>
2020-01-10 10:08:20 +0100

    Pequeño cambio en lagersts controller para facilitar prueba de inserccion

--- router/controllers/lagerSts.js
+++ router/controllers/lagerSts.js
@@ -19,16 +19,16 @@ module.exports = (database, lagerSts) => {
     const sql = lagerSts.insertOne();
     console.log(sql);
     console.log(req.query)
-    let lag_mandant = req.query.lag_mandant;
-    let lag_art_typ = parseInt(req.query.lag_art_typ);
-    let lag_art_num = parseInt(req.query.lag_art_num);
-    let lag_bestand = parseInt(req.query.lag_bestand);
-    let lag_res_bestand = parseInt(req.query.lag_res_bestand);
+    let lag_mandant = (req.query.lag_mandant || null);
+    let lag_art_typ = (parseInt(req.query.lag_art_typ) || null);
+    let lag_art_num = (parseInt(req.query.lag_art_num) || null);
+    let lag_bestand = (parseInt(req.query.lag_bestand) || null);
+    let lag_res_bestand = (parseInt(req.query.lag_res_bestand) || null);
     let lag_referenz = parseInt(req.query.lag_referenz);
-    let lag_breite = parseInt(req.query.lag_breite);
-    let lag_hoehe = parseInt(req.query.lag_hoehe);
-    let lag_mat_code = req.query.lag_mat_code;
-    let lag_ltzt_ein_dat = req.query.lag_ltzt_ein_dat;
+    let lag_breite = (parseInt(req.query.lag_breite) || null);
+    let lag_hoehe = (parseInt(req.query.lag_hoehe) || null);
+    let lag_mat_code = (req.query.lag_mat_code || null);
+    let lag_ltzt_ein_dat = (req.query.lag_ltzt_ein_dat || null);
     database.open(sql, [lag_mandant, lag_art_typ, lag_art_num, lag_bestand, lag_res_bestand, lag_referenz, lag_breite,
     lag_hoehe, lag_mat_code, lag_ltzt_ein_dat], true, res);
   };
